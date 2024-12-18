78d85d571b6d11898d1eb2211fad59d023c6e9fe
Humeira Iqbal <humeiraiqbal1@gmail.com>
2024-12-18 22:24:57 +0500

    Add files via upload

---
+++ page.tsx
@@ -0,0 +1,17 @@
+import Link from "next/link";
+
+export default function Home(){
+    return (
+       
+            
+      <div className="bg-red-400 h-screen flex text-white">
+    
+      <div>LOGO</div>
+      
+        {/* <div className="bg-purple-400 w-1/2 h-48"> */}
+        <div className="cursor Pointer">Home</div>
+                
+      </div>
+      
+    );
+  };
